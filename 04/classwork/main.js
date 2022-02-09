9dc503de3935ee89a6215c49672d8029802dff99
Denys Kovalov <97830456+kovalyovds@users.noreply.github.com>
2022-02-09 22:55:38 +0200

    up

--- 04/classwork/main.js
+++ 04/classwork/main.js
@@ -58,10 +58,92 @@
 //
 // console.log(foo(arr));
 
-// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+//   Приклад [1,2,10]->13
+
+// let arr = [10, 30, 10, 60, 70, 40, 20, 40, 20];
+//
+// function foo(array) {
+//     let sum = 0;
+//     for (let arrayElement of array) {
+//         sum += arrayElement;
+//     }
+//     return sum;
+// }
+//
+// console.log(foo(arr));
+
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+
+// let arr = [10, 30, 10, 60, 70, 40, 20, 40, 20, 50];
+//
+// function foo(array) {
+//     let sum = 0;
+//     for (let arrayElement of array) {
+//         sum += arrayElement;
+//     }
+//     return sum / array.length;
+// }
+//
+// console.log(foo(arr));
+
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
+//   (Math використовувати заборонено);
+
+// function foo(arg) {
+//     let min = arguments[0];
+//     let max = arguments[0];
+//     for (let argElement of arguments) {
+//         if (argElement > max) {
+//             max = argElement;
+//         }
+//         if (argElement < min) {
+//             min = argElement;
+//         }
+//     }
+//     console.log(max);
+//     return min;
+// }
+//
+// console.log('min', foo(4, 5, 6, 3, 2, 5, 6, 8, 1));
+
 // - створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
-// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+
+// function foo(length) {
+//     let arr = [];
+//     for (let i = 0; i < length; i++) {
+//         arr[i] = Math.floor(Math.random() * 100);
+//     }
+//     return arr
+// }
+//
+// console.log(foo(4));
+
+// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit.
+//   limit - аргумент, який характеризує кінцеве значення діапазону.
+
+// function foo(length, limit) {
+//     let arr = [];
+//     for (let i = 0; i < length; i++) {
+//         arr[i] = Math.floor(Math.random() * limit);
+//     }
+//     return arr
+// }
+//
+// console.log(foo(4, 10));
+
+
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+
+// let qq = [1, 2, 3];
+//
+// function reverse(arr) {
+//     let newArr = [];
+//     for (let i = arr.length - 1, ri = 0; i >= 0; i--, ri++) {
+//         newArr[ri] = arr[i];
+//     }
+//     return newArr;
+// }
+//
+// console.log(reverse(qq));
